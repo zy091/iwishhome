f3a0adf0ebb369aabb29d79f0bf7fd76cda7ef2e
zy091 <114466057+zy091@users.noreply.github.com>
2025-07-29 11:10:42 +0800

    Delete dist/assets directory

--- dist/assets/Customer-DfNz-FpN.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index-02dAV7KX.js";import{y as o,A as t,z as n}from"./vendor-COQJj1AX.js";import"./elementPlus-Dv7g5dxn.js";import"./supabase-BNvKcJjS.js";const s={};function a(c,e){return n(),o("div",null,e[0]||(e[0]=[t("h1",null,"客户管理",-1)]))}const p=r(s,[["render",a]]);export{p as default};
